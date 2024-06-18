c2914de49c4f92f730c9eea567303701d8a09c01
devfrend <d.dumitrascuta@gmail.com>
2024-06-18 10:05:52 +0100

    skip first 2 lines in the file

--- src/readFile.ts
+++ src/readFile.ts
@@ -2,10 +2,9 @@ import fs from 'fs'
 
 const buffer = fs.readFileSync('project/lines')
 
-const lines = buffer
+buffer
   .toString()
   .split('\n')
   .filter((_, i) => i % 2 === 0)
+  .filter((_, i) => i > 1 && i < 4)
   .forEach((l) => console.log(l))
-
-console.log(lines)
