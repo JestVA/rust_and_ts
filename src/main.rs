b6202ac1aa18d4477b78d6d729ff9822e2c6c80a
devfrend <d.dumitrascuta@gmail.com>
2024-07-02 21:42:48 +0100

    impl, trait and structs

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,80 @@
+//#![allow(dead_code)]
+//struct Custom {
+//    age: usize,
+//    name: String
+//}
+
+//// doing this at a language level! Great (no type guards required like in Ts)
+//enum Item {
+//    Number(usize),
+//    String(String),
+//    MyCustom(Custom),
+//}
+
+//fn append(items: &mut Vec<Item>) {
+//    items.push(Item::String("Hello again".to_string())); // make a string here <---
+//}
+
+//fn main() {
+//    let mut items : Vec<Item> = vec![];
+//    append(&mut items);   
+
+//    // calling some or none
+//    let _foo = Some(5);
+//    let _foo : Option<String> = None;
+
+//}
+
+
+
+// Exploring traits and interfaces
 #![allow(dead_code)]
-struct Custom {
-    age: usize,
-    name: String
+
+use std::f64::consts::PI;
+
+struct Rect {
+    x: f64,
+    y: f64,
+    width: f64,
+    height: f64,
 }
 
-// doing this at a language level! Great (no type guards required like in Ts)
-enum Item {
-    Number(usize),
-    String(String),
-    MyCustom(Custom),
+struct Circle {
+    x: f64,
+    y: f64,
+    radius: f64
 }
 
-fn append(items: &mut Vec<Item>) {
-    items.push(Item::String("Hello again".to_string())); // make a string here <---
+trait Area {
+    fn area(&self) -> f64;
+}
+
+impl Area for Rect {
+    fn area(&self) -> f64 {
+        return self.width * self.height
+    }
+}
+
+impl Area for Circle {
+    fn area(&self) -> f64 {
+        return self.radius * self.radius * PI
+    }
 }
 
 fn main() {
-    let mut items : Vec<Item> = vec![];
-    append(&mut items);   
+    let rect = Rect {
+        x: 0.0,
+        y: 0.0,
+        width: 10.0,
+        height: 10.0
+    };
 
-    // calling some or none
-    let _foo = Some(5);
-    let _foo : Option<String> = None;
+    let circ = Circle {
+        x: 0.0,
+        y: 0.0,
+        radius: 10.0
+    };
 
+    println!("{}", circ.area());
+    println!("{}", rect.area());
 }
