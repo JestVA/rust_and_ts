9311cc8195c1794cf0464d1b454c38f4d94a550a
devfrend <d.dumitrascuta@gmail.com>
2024-07-02 22:13:06 +0100

    play with module system, split into multiple files and import structs and traits

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,11 @@
-//#![allow(dead_code)]
-//struct Custom {
-//    age: usize,
-//    name: String
-//}
+use crate::shapes::{rect::Rect, circle::Circle, area::Area};
 
-//// doing this at a language level! Great (no type guards required like in Ts)
-//enum Item {
-//    Number(usize),
-//    String(String),
-//    MyCustom(Custom),
-//}
-
-//fn append(items: &mut Vec<Item>) {
-//    items.push(Item::String("Hello again".to_string())); // make a string here <---
-//}
-
-//fn main() {
-//    let mut items : Vec<Item> = vec![];
-//    append(&mut items);   
-
-//    // calling some or none
-//    let _foo = Some(5);
-//    let _foo : Option<String> = None;
-
-//}
+/*
+Exploring traits and interfaces
+*/
+mod shapes;
 
 
-
-// Exploring traits and interfaces
-#![allow(dead_code)]
-
-use std::f64::consts::PI;
-
-struct Rect {
-    x: f64,
-    y: f64,
-    width: f64,
-    height: f64,
-}
-
-struct Circle {
-    x: f64,
-    y: f64,
-    radius: f64
-}
-
-trait Area {
-    fn area(&self) -> f64;
-}
-
-impl Area for Rect {
-    fn area(&self) -> f64 {
-        return self.width * self.height
-    }
-}
-
-impl Area for Circle {
-    fn area(&self) -> f64 {
-        return self.radius * self.radius * PI
-    }
-}
 
 fn main() {
     let rect = Rect {
---
+++ src/shapes/circle.rs
@@ -0,0 +1,16 @@
+use super::area::Area;
+
+use std::f64::consts::PI;
+
+#[allow(dead_code)]
+pub(crate) struct Circle {
+    pub x: f64,
+    pub y: f64,
+    pub radius: f64
+}
+
+impl Area for Circle {
+    fn area(&self) -> f64 {
+        return self.radius * self.radius * PI
+    }
+}
