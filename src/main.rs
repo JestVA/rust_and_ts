6e68e675e1d5b70fe87c2fb6d6779b8ffec5c0f0
devfrend <d.dumitrascuta@gmail.com>
2024-07-04 21:07:21 +0100

    mandatory Rust Fizzbuzz

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,15 @@ fn main() -> Result<()> {
    // println!("Hello world of Rustz...!"); 
    // Howdy boyz and girlz. Using anyhow to return a better Result type which can be reasonably
    // used in return types for main functions and in general throughout Rust code
-   Ok(())
+
+    for i in 1..=100 {
+        match i {
+            n if n % 3 == 0 && n % 5 == 0 => println!("FizzBuzz"),
+            n if n % 3 == 0 => println!("Fizzbuzz"),
+            n if n % 5 == 0 => println!("BUzz"),
+            n => println!("{n}"),
+        }
+    }
+
+    Ok(())
 }
