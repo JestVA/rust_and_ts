fd54b0e9ee5381cd91f04500d3f53f33f82325d2
devfrend <d.dumitrascuta@gmail.com>
2024-07-04 19:32:01 +0100

    removed more shapes, neovim hello world

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,3 @@
-use crate::shapes::{area::Area, circle::Circle, rect::Rect};
-
-/*
-Exploring traits and interfaces
-*/
-mod shapes;
-
 fn main() {
-    let rect = Rect {
-        x: 0.0,
-        y: 0.0,
-        width: 10.0,
-        height: 10.0,
-    };
-
-    let circ = Circle {
-        x: 0.0,
-        y: 0.0,
-        radius: 10.0,
-    };
-
-    println!("{}", circ.area());
-    println!("{}", rect.area());
+   println!("Hello world of Rustz...!"); 
 }
