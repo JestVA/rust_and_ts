f3b2860b4b703bad86c571cb1d1535c3619db428
devfrend <d.dumitrascuta@gmail.com>
2024-07-04 19:52:24 +0100

    more tinkering with Intellisense

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-   println!("Hello world of Rustz...!"); 
+use anyhow::Result;
+
+fn main() -> Result<()> {
+   // println!("Hello world of Rustz...!"); 
+   // Howdy boyz and girlz. Using anyhow to return a better Result type which can be reasonably
+   // used in return types for main functions and in general throughout Rust code
+   Ok(())
 }
